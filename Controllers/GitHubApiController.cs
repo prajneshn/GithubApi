308a35e5a96d2d26f3b05c734b3a81c30eb4e5dc
Prajnesh Noochila <prnoo@eg.dk>
2025-09-28 19:05:03 +0530

    github start implementation

---
+++ Controllers/GitHubApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GithubApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitHubApiController(Interfaces.GithubApi api) : ControllerBase
+    {
+        [HttpGet("pullrequests/{owner}/{repo}")]
+        public IActionResult GetPullRequests(string owner, string repo)
+        {
+            var pullRequests = api.GetPullRequests(owner, repo);
+            return Ok(pullRequests);
+        }
+    }
+}
---
+++ Services/GithubApiService.cs
@@ -0,0 +1,31 @@
+﻿using GithubApi.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GithubApi.Services
+{
+    public class GithubApiService(HttpClient httpClient) : Interfaces.GithubApi
+    {
+        public async Task<List<PullRequestDto>> GetPullRequests(string owner, string repo)
+        {
+            httpClient.DefaultRequestHeaders.UserAgent.Add(
+                new ProductInfoHeaderValue("MyApp", "1.0")); // Required by GitHub
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("token", "");
+
+            var url = $"https://api.github.com/repos/{owner}/{repo}/pulls?state=closed";
+
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            var prs = JsonSerializer.Deserialize<List<PullRequestDto>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            return prs ?? new List<PullRequestDto>();
+        }
+    }
+}
